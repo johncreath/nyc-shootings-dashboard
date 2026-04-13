4c8576d872fb637ff2ca086a1eecba424b24b88a
John Creath <johncreath@gmail.com>
2026-04-13 06:46:04 -0500

    Adds data load function to app.py

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ st.set_page_config(page_title="NYC Shootings Dashboard", layout="wide")
 st.title("NYC Shootings Dashboard")
 
 st.write("If you can see this, Streamlit is working.")
+
+from src.load_data import load_data
+
+df = load_data()
+
+st.write(df.head())
